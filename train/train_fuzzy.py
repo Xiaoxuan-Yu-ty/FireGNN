3ecb99c1d1eb1f2a813d86f83dccb2e63874b3e2
Xiaoxuan-Yu-ty <ytxiaoxuan@gmail.com>
2026-03-02 09:23:25 +0100

    update train_fuzzy

--- train/train_fuzzy.py
+++ train/train_fuzzy.py
@@ -130,9 +130,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='fuzzy_only',
                         choices=['gcn', 'gat', 'gin','paper_gcn', 'fuzzy_only'])
-    parser.add_argument('--dataset', type=str, default='Composite', choices=['Composite', 'Composite','NormExpression','RawExpression'])
+    parser.add_argument('--dataset', type=str, default='NormExpression', choices=['Composite', 'Composite','NormExpression','RawExpression'])
     parser.add_argument('--k', type=int, default=5, help="k used in K-NN clustering to build graph")
-    parser.add_argument('--graph_file', type=str, default="../AD/data/composite_patient_k5.pkl")
+    parser.add_argument('--graph_file', type=str, default="../AD/data/normalized_expression_k5.pkl")
     parser.add_argument('--output_dir', type=str, default='../results')
     parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--hidden_channels', type=int, default=64)
